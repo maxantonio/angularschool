35a6fbc1c81af5a454a9caa8900ca7682a29278b
Example <dayrobis@jovenclub.cu>
2015-03-23 15:43:02 -0400

    inicializando repo

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+    <script src= "angular-1.3.15/angular.min.js"></script>
+</head>
+
+<body>
+
+<div ng-app="">
+
+    <p>Input something in the input box:</p>
+    <p>Name: <input type="text" ng-model="name"></p>
+    <p>{{name}}</p>
+
+</div>
+
+</body>
+</html>
